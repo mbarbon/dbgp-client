ac983d4f04ad4ff57ebf64fdfb0f8c12d7a862bf
Mattia Barbon <mattia@barbon.org>
2015-12-13 13:06:18 +0100

    Add basic documentation

--- lib/DBGp/Client.pm
+++ lib/DBGp/Client.pm
@@ -7,8 +7,55 @@ use warnings;
 
 DBGp::Client - simple client for the DBGp debugger protocol
 
+=head1 SYNOPSIS
+
+    $listener = DBGp::Client::Listener->new(port => 9000);
+    $listener->listen;
+
+    while (my $client = $listener->accept) {
+        # set a conditional breakpoint that never triggers,
+        # but has side-effects in the condition
+        $command = 'require Data::Dumper; print Data::Dumper::Dumper($var);';
+        $res = $client->send_command(
+            'breakpoiint_set', '-t', 'conditional',
+                               '-f', 'file:///path/to/file.pl',
+                               '-n', $line,
+                               '--',
+                               encode_base64("$command; 0"),
+        );
+        die $res->message if $res->is_error;
+
+        # continue execution
+        $res = $client->send_command('run');
+        die $res->message if $res->is_error;
+    }
+
+=head1 DESCRIPTION
+
+A simple client for the DBGp debugger protocol; it can be used for
+testing a debugger implementation or scripting a program through the
+debugger interface.
+
+See L<DBGp::Client::Listener> and L<DBGp::Client::Connection> for API
+documentation.
+
 =cut
 
 our $VERSION = '0.02';
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Mattia Barbon <mbarbon@cpan.org>
+
+=head1 LICENSE
+
+Copyright (c) 2015 Mattia Barbon. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
--- lib/DBGp/Client/Connection.pm
+++ lib/DBGp/Client/Connection.pm
@@ -3,8 +3,47 @@ package DBGp::Client::Connection;
 use strict;
 use warnings;
 
+=head1 NAME
+
+DBGp::Client::Connection - DBGp connection class
+
+=head1 SYNOPSIS
+
+    $connection = $listener->accept;
+
+    $res = $connection->send_command('step_over');
+    die $res->message if $res->is_error;
+
+    $res = $connection->send_command('eval', '--', encode_base64('$var'));
+    die $res->message if $res->is_error;
+
+    # assumes result is a scalar value, it should check ->children
+    print $res->result->value, "\n";
+
+=head1 DESCRIPTION
+
+Simple blocking interface for a DBGp connection.
+
+=head1 METHODS
+
+=cut
+
 use DBGp::Client::Stream;
 use DBGp::Client::Parser;
+
+=head2 new
+
+    $connection = DBGp::Client::Connection->new(
+        socket  => $connected_socket,
+    );
+
+Usually called by L<DBGp::Client::Listener>, not used directly.
+
+Creates a new connection object wrapping the passed-in socket; after
+construction, call L</parse_init> to process the initialization message
+sent by the debugger.
+
+=cut
 
 sub new {
     my ($class, %args) = @_;
@@ -18,11 +57,37 @@ sub new {
     return $self;
 }
 
+=head2 parse_init
+
+    $init = $connection->parse_init;
+
+Usually called by L<DBGp::Client::Listener>, not used directly.
+
+Parses the init message sent by the debugger, and returns a
+L<DBGp::Client::Response/init> object.
+
+=cut
+
 sub parse_init {
     my ($self) = @_;
 
     $self->{init} = DBGp::Client::Parser::parse($self->{stream}->get_line);
 }
+
+=head2 send_command
+
+    $res = $connection->send_command('step_over');
+    $res = $connection->send_command('eval', '--', 'base64-encoded-data');
+
+Sends a command to the debugger, parses the answer and returns it as a
+response object (see L<DBGp::Client::Response>).
+
+It automatically adds the DBGp transaction id (C<-i> parameter) to the
+command.
+
+Note that this method could block indefinitely.
+
+=cut
 
 sub send_command {
     my ($self, $command, @args) = @_;
@@ -38,3 +103,18 @@ sub send_command {
 }
 
 1;
+
+__END__
+
+=head1 AUTHOR
+
+Mattia Barbon <mbarbon@cpan.org>
+
+=head1 LICENSE
+
+Copyright (c) 2015 Mattia Barbon. All rights reserved.
+
+This library is free software; you can redistribute it and/or modify it
+under the same terms as Perl itself.
+
+=cut
